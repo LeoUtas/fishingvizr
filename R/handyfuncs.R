c5984a95c963bdfb11b3363239f60b57c929f1ec
LeoUTas <hoang.nguyen0@utas.edu.au>
2023-03-30 14:15:42 -0230

    move everything in makeinputfuncs.R to handyfuncs.R, and create some more handy functions to use for data visualization possibly can be used with rayshayder

--- R/handyfuncs.R
+++ R/handyfuncs.R
@@ -1,5 +1,7 @@
 # ---------- SOME HANDY FUNCTIONS FOR MY PHD WORK ----------- #
 
+
+# ---------- FUNCTION 1 ----------- #
 # this chunk of codes works the same way the "Session => Set Working Directory => To Source File Location" does
 get_backto_WD = function() {
   {
@@ -17,6 +19,7 @@ get_backto_WD = function() {
 }
 
 
+# ---------- FUNCTION 2 ----------- #
 # This chunk of codes is to check if there is any repetitive locations (i.e., same lat and long)
 test_repetitive_locs = function(data) {
 
@@ -37,6 +40,8 @@ test_repetitive_locs = function(data) {
   }
 }
 
+
+# ---------- FUNCTION 3 ----------- #
 # to make effortdens_rt matrix
 make_effortdens_rt = function(data, Tri_Area = TriList$Tri_Area) {
 
@@ -83,6 +88,8 @@ make_effortdens_rt = function(data, Tri_Area = TriList$Tri_Area) {
 
 }
 
+
+# ---------- FUNCTION 4 ----------- #
 # to fit the model
 fit_the_model = function(tmb_data, tmb_pars) {
 
@@ -130,14 +137,16 @@ fit_the_model = function(tmb_data, tmb_pars) {
 }
 
 
-
-
-
+# ---------- FUNCTION 5 ----------- #
 # create a list of polygons
-{
+make_polygons = function (data, n_loop) {
+
+  n_loop = n_loop # n_loop = the number of triangles
+  V_ls = data # data are TriList$V0, TriList$V2, TriList$V2
+
   mat_ls = list()
   poly_ls = list()
-  for (i in 1:length(TriList$Tri_Area)) {
+  for (i in 1:n_loop) {
 
     mat_ls[[i]] =
       as.matrix(
@@ -153,6 +162,87 @@ fit_the_model = function(tmb_data, tmb_pars) {
   }
   crs <- st_crs("+proj=longlat +datum=WGS84")
   poly_sf <- st_sf(geometry = st_sfc(poly_ls), crs = crs)
+
+  return(poly_sf)
+
+}
+
+
+# ---------- FUNCTION 6 ----------- #
+make_tri_maps = function (data, n_loop, effort_data, n_breaks, selected_region,
+                          legend_title = "Effort",
+                          plot_title = "Title") {
+
+  data = data # data are TriList$V0, TriList$V2, TriList$V2
+  n_loop = n_loop # n_loop = the number of triangles
+
+  poly_sf = make_polygons(data = data, n_loop = n_loop)
+
+  effortdens_rt = effort_data # take in the effort data in a matrix form
+  n_breaks = n_breaks # the number of breaks for legend scale
+  selected_region = selected_region # use the FAO code
+  legend_title = legend_title
+  plot_title = plot_title
+
+  # arrange inputs for the loop creating triangle plots
+  {
+    limits = range(effortdens_rt)
+    breaks = pretty(c(0,max(effortdens_rt)), n = n_breaks)
+    poly_sf_ls <- list()
+    filtered_poly_sf_ls = list()
+    Tri_plot_ls = list()
+    Output_ls = list()
+
+    # run to select a mapping region
+    selected_map_region = subset(shp_source, F_CODE %in% selected_region)
+  }
+
+  # Loop to create each poly_sf object, append it to the list, and generate triangle plots
+  for (i in 1:ncol(effortdens_rt)) {
+    # append to the list
+    poly_sf_ls[[i]] <- poly_sf # e.g., poly_sf_ls[[1]] = poly_sf_ls in the year 2012
+    # add the effort attribute to the sf object for year i
+    poly_sf_ls[[i]]$effort = effortdens_rt[,i]
+    # filter the sf object to use the data within the selected map only
+    filtered_poly_sf_ls[[i]] = st_intersection(poly_sf_ls[[i]], selected_map_region)
+
+    # plot the triangles of efforts
+    Tri_plot_ls[[i]] = ggplot() +
+
+      geom_sf(data = (filtered_poly_sf_ls[[i]]),
+              alpha = .8,
+              aes(fill=effort),
+              color = "white") +
+
+      scale_fill_gradientn(colors = texture,
+                           limits = limits,
+                           breaks = breaks) +
+
+      labs(fill = paste(legend_title, "\n")) +
+
+      ggtitle(paste(plot_title,
+                    colnames(effortdens_rt)[i], sep = " ")) +
+
+      theme(panel.grid = element_blank(),
+            plot.background = element_rect(fill = "white"),
+            panel.background = element_rect(fill = "white"),
+            panel.border = element_rect(color = "darkgrey", fill = NA, linewidth = 1),
+            #legend.position = c(0.2, 0.4)
+
+            legend.direction = "horizontal",
+            legend.position = "bottom",
+            legend.box = "horizontal",
+            legend.margin = margin(t = 0, r = 0, b = 0, l = 0),
+            legend.text.align = 0,
+            legend.title.align = 0
+      )
+
+    # return a list of filtered_poly_sf_ls and Tri_plot_ls
+    Output_ls[[i]] = list(filtered_poly_sf_ls[[i]], Tri_plot_ls[[i]])
+  }
+
+  return(Output_ls)
+
 }
 
 
@@ -166,9 +256,7 @@ fit_the_model = function(tmb_data, tmb_pars) {
 
 
 
-
-
-# ------------------ TEST CODE ------------------ #
+# ------------------ TEST CODE FOR EFFORTDENS_RT ------------------ #
 # year_test = c(2001:2002,2003,2003,2004:2006,2006,2006,2007,2007:2010,2010,2010)
 # r_i_test = c(4,5,5,6,1:2,3,3,3,4,6:8,8,9,9)
 # value_test = c(1.5:16.5)
